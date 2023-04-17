7a608beea064c103a868de74daf91b618f0ddbb9
nabhansuwanachote <nabhan.au@gmail.com>
2023-04-17 14:16:20 +0700

    feat: init main.go

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/GarnBarn/common-go/database"
+	"github.com/GarnBarn/common-go/httpserver"
+	"github.com/GarnBarn/common-go/logger"
+	"github.com/GarnBarn/gb-tag-service/config"
+	"github.com/sirupsen/logrus"
+)
+
+var appConfig config.Config
+
+func init() {
+	appConfig = config.Load()
+	logger.InitLogger(logger.Config{
+		Env: appConfig.Env,
+	})
+
+}
 
 func main() {
-	fmt.Println("HelloWorld")
+	// Start DB Connection
+	_, err := database.Conn(appConfig.MYSQL_CONNECTION_STRING)
+	if err != nil {
+		logrus.Panic("Can't connect to db: ", err)
+	}
+
+	// Create the http server
+	httpServer := httpserver.NewHttpServer()
+
+	logrus.Info("Listening and serving HTTP on :", appConfig.HTTP_SERVER_PORT)
+	httpServer.Run(fmt.Sprint(":", appConfig.HTTP_SERVER_PORT))
 }
